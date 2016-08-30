48b174778f39e42f18734365448db2d5e1dbc879
manish8856 <manish.kumar@crichq.com>
2016-08-30 16:56:31 +0530

    update

--- ES/recipes/default.rb
+++ ES/recipes/default.rb
@@ -7,5 +7,10 @@ execute "install java" do
   command "wget --no-check-certificate --no-cookies --header 'Cookie: oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.tar.gz"
 end
 
+execute "extract_some_tar" do
+  cwd "/tmp"
+  command "tar -xzvf jdk-8u102-linux-x64.tar.gz"
+  not_if { File.exists?("/tmp") }
+end
 
 
