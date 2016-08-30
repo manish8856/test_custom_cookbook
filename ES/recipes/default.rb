e9aec4be0dbb85ecbf10b85ff5fcb28fa865a769
manish8856 <manish.kumar@crichq.com>
2016-08-30 15:55:54 +0530

    update

--- ES/recipes/default.rb
+++ ES/recipes/default.rb
@@ -1,19 +1,11 @@
 execute "install java" do
-command "echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
-echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list"
+command "sudo apt-get install -y python-software-properties debconf-utils
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer"
 end
 
-execute "install java" do
-command "apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886"
-end
-
-execute "install java" do
-command "apt-get update -yes"
-end
-
-execute "install java" do
-command "apt-get install --yes oracle-java7-installer"
-end
 
 
 
