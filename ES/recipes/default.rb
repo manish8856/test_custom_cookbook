0becd8ac11b4e4e065efbe1f5cf97cd67b01f8ad
manish8856 <manish.kumar@crichq.com>
2016-08-30 16:21:17 +0530

    update

--- ES/recipes/default.rb
+++ ES/recipes/default.rb
@@ -4,7 +4,7 @@ end
 
 execute "install java" do
 cwd "/tmp"
-command "wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.tar.gz"
+command "wget --no-check-certificate --no-cookies --header"Cookie: oraclelicense=accept-securebackup-cookie"http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.tar.gz"
 end
 
 
