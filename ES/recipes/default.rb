e2b3451ffd17ad2bb2369a35dd2ee50677ddc566
manish8856 <manish.kumar@crichq.com>
2016-08-30 16:02:07 +0530

    update

--- ES/recipes/default.rb
+++ ES/recipes/default.rb
@@ -1,9 +1,20 @@
 execute "install java" do
-command "sudo apt-get install -y python-software-properties debconf-utils
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo apt-get update
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-sudo apt-get install -y oracle-java8-installer"
+command "sudo apt-get install -y python-software-properties debconf-utils"
+end
+
+execute "install java" do
+command "sudo add-apt-repository -y ppa:webupd8team/java"
+end
+execute "install java" do
+command "sudo apt-get update -y"
+end
+
+execute "install java" do
+command "echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections"
+end
+
+execute "install java" do
+command "sudo apt-get install -y oracle-java8-installer"
 end
 
 
