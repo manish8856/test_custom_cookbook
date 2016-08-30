c27de8a71ba2aac14664494527f465bc787331b5
manish8856 <manish.kumar@crichq.com>
2016-08-30 16:26:03 +0530

    update

--- ES/recipes/default.rb
+++ ES/recipes/default.rb
@@ -1,10 +1,10 @@
 execute "install wget" do
-command "sudo apt-get install -y wget"
+  command "sudo apt-get install -y wget"
 end
 
 execute "install java" do
-cwd "/tmp"
-command "wget --no-check-certificate --no-cookies --header"Cookie: oraclelicense=accept-securebackup-cookie"http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.tar.gz"
+  cwd "/tmp"
+  command "wget --no-check-certificate --no-cookies --header"Cookie: oraclelicense=accept-securebackup-cookie"http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.tar.gz"
 end
 
 
