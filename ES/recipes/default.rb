6007b292fea6d481dc4123bd81d6fd1bfebce51e
manish8856 <manish.kumar@crichq.com>
2016-08-30 15:49:55 +0530

    update

--- ES/recipes/default.rb
+++ ES/recipes/default.rb
@@ -1,9 +1,18 @@
 execute "install java" do
-  command "echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
-echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
-apt-get update
-apt-get install --yes oracle-java7-installer"
+command "echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list"
+end
+
+execute "install java" do
+command "apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886"
+end
+
+execute "install java" do
+command "apt-get update -yes"
+end
+
+execute "install java" do
+command "apt-get install --yes oracle-java7-installer"
 end
 
 
